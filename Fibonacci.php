2bc9a7cadaaae5cb2cf04030985068ded62d68b4
steamgryshan1998 <35163053+steamgryshan1998@users.noreply.github.com>
2020-04-11 23:09:34 +0300

    Update Fibonacci.php

--- Fibonacci.php
+++ Fibonacci.php
@@ -1,12 +1,12 @@
 <?php
-// Fibonacci
+// create a function that outputting the value by number in Fibonacci numbers
     function Fibonacci($f){
-        $fib = array(0,1);
+        $fib = array(0,1);//setting the starting values of the Fibonacci numbers
         for($i = 2; $i <= $f; $i++){
-            $fib[$i] = $fib[$i-1] + $fib[$i-2];
+            $fib[$i] = $fib[$i-1] + $fib[$i-2];//calculation the value by certain number
         }
-        echo $fib[$f]."\n";
+        echo $fib[$f]."\n";//"\n" - newline
     }
     
-    Fibonacci(8);
+    Fibonacci(8);//calling a function
 ?>
