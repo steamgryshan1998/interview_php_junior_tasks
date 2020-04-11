d14b976324945e69e1a19ea25520693228d0300e
steamgryshan1998 <35163053+steamgryshan1998@users.noreply.github.com>
2020-04-11 23:05:34 +0300

    Update Anagram.php

--- Anagram.php
+++ Anagram.php
@@ -1,15 +1,15 @@
 <?php
-// Anagramm
+// create a function that checks the words for the same letters the same number of times
     function Anagram($a, $b){
-        $a = mb_strtolower($a);
+        $a = mb_strtolower($a);//Making a string lowercase
         $b = mb_strtolower($b);
-        $ar = str_split($a);
+        $ar = str_split($a);// Converting string to the array
         $br = str_split($b);
-        for($i = 0; $i < strlen($a); $i++){
-            if(in_array($ar[$i], $br)){
-                continue;
+        for($i = 0; $i < strlen($a); $i++){//strlen($a) - the length of the string $a
+            if(in_array($ar[$i], $br)){//Checks if a value is in the array
+                continue;//skipping the remainder of the current iteration 
             }  else {
-                echo "False\n";
+                echo "False\n";//"\n" - newline
                 return false;
             }
         }
@@ -17,6 +17,6 @@
         return true;
     }
     
-    Anagram("Friend","inder");
+    Anagram("Friend","inder");//Calling a function
     Anagram("true","true");
 ?>
