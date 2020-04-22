70935b29123d9541a59c166f11c0b550b732992e
steamgryshan1998 <35163053+steamgryshan1998@users.noreply.github.com>
2020-04-22 16:35:38 +0300

    Update MaxProfit.php

--- MaxProfit.php
+++ MaxProfit.php
@@ -1,10 +1,11 @@
 <?php
-// Your code here!
-    function MaxProfit(&$arr){
-        $max = NULL;
-        for($i = 1; $i < count($arr); $i++){
-            for($j = $i-1; $j >= 0; $j--){
-                if($arr[$i] - $arr[$j] > $max){
+// You have a list of stock prices for N days(in the form of an array), 
+// you need to determine the maximum profit for one purchase and sale operation 
+    function MaxProfit(&$arr){//
+        $max = NULL;//initialization of variable
+        for($i = 1; $i < count($arr); $i++){//going alone the array(list of stock prices)
+            for($j = $i-1; $j >= 0; $j--){//going back to get the values of difference
+                if($arr[$i] - $arr[$j] > $max){//checking the current difference with the maximum 
                     $max = $arr[$i] - $arr[$j];
                 }
             }
@@ -13,6 +14,6 @@
     }
     
     $a = array(9, 6, 13, 10, 20, 2);
-    MaxProfit($a);
+    MaxProfit($a);//calling a function
     $b = array(20, 11, 10, 8, 5, 2);
     MaxProfit($b);
